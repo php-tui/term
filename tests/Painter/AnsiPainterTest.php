03dc19521d542decdc54366a09e6ce47521be370
Daniel Leech <daniel@dantleech.com>
2023-11-27 11:47:25 +0000

    Painter ok

--- tests/Painter/AnsiPainterTest.php
+++ tests/Painter/AnsiPainterTest.php
@@ -17,46 +17,57 @@ final class AnsiPainterTest extends TestCase
 {
     public function testControlSequences(): void
     {
-        $this->assertAnsiCode('6n', Actions::requestCursorPosition());
-        $this->assertAnsiCode('42m', Actions::setBackgroundColor(Colors::Green));
-        $this->assertAnsiCode('34m', Actions::setForegroundColor(Colors::Blue));
-        $this->assertAnsiCode('48;2;2;3;4m', Actions::setRgbBackgroundColor(2, 3, 4));
-        $this->assertAnsiCode('38;2;2;3;4m', Actions::setRgbForegroundColor(2, 3, 4));
-        $this->assertAnsiCode('?25l', Actions::cursorHide());
-        $this->assertAnsiCode('?25h', Actions::cursorShow());
-        $this->assertAnsiCode('?1049h', Actions::alternateScreenEnable());
-        $this->assertAnsiCode('?1049l', Actions::alternateScreenDisable());
-        $this->assertAnsiCode('2;3H', Actions::moveCursor(2, 3));
-        $this->assertAnsiCode('0m', Actions::reset());
-        $this->assertAnsiCode('1m', Actions::bold(true));
-        $this->assertAnsiCode('2m', Actions::dim(true));
-        $this->assertAnsiCode('3m', Actions::italic(true));
-        $this->assertAnsiCode('23m', Actions::italic(false));
-        $this->assertAnsiCode('4m', Actions::underline(true));
-        $this->assertAnsiCode('24m', Actions::underline(false));
-        $this->assertAnsiCode('5m', Actions::slowBlink(true));
-        $this->assertAnsiCode('7m', Actions::reverse(true));
-        $this->assertAnsiCode('8m', Actions::hidden(true));
-        $this->assertAnsiCode('9m', Actions::strike(true));
-        $this->assertAnsiCode('2J', Actions::clear(ClearType::All));
-        $this->assertAnsiCode('3J', Actions::clear(ClearType::Purge));
-        $this->assertAnsiCode('J', Actions::clear(ClearType::FromCursorDown));
-        $this->assertAnsiCode('1J', Actions::clear(ClearType::FromCursorUp));
-        $this->assertAnsiCode('2K', Actions::clear(ClearType::CurrentLine));
-        $this->assertAnsiCode('K', Actions::clear(ClearType::UntilNewLine));
-        $this->assertAnsiCode('S', Actions::scrollUp());
-        $this->assertAnsiCode('T', Actions::scrollDown());
-        $this->assertAnsiCode("0;Hello\x07", Actions::setTitle('Hello'));
+        $this->assertCsiSeq('6n', Actions::requestCursorPosition());
+        $this->assertCsiSeq('42m', Actions::setBackgroundColor(Colors::Green));
+        $this->assertCsiSeq('34m', Actions::setForegroundColor(Colors::Blue));
+        $this->assertCsiSeq('48;2;2;3;4m', Actions::setRgbBackgroundColor(2, 3, 4));
+        $this->assertCsiSeq('38;2;2;3;4m', Actions::setRgbForegroundColor(2, 3, 4));
+        $this->assertCsiSeq('?25l', Actions::cursorHide());
+        $this->assertCsiSeq('?25h', Actions::cursorShow());
+        $this->assertCsiSeq('?1049h', Actions::alternateScreenEnable());
+        $this->assertCsiSeq('?1049l', Actions::alternateScreenDisable());
+        $this->assertCsiSeq('2;3H', Actions::moveCursor(2, 3));
+        $this->assertCsiSeq('0m', Actions::reset());
+        $this->assertCsiSeq('1m', Actions::bold(true));
+        $this->assertCsiSeq('2m', Actions::dim(true));
+        $this->assertCsiSeq('3m', Actions::italic(true));
+        $this->assertCsiSeq('23m', Actions::italic(false));
+        $this->assertCsiSeq('4m', Actions::underline(true));
+        $this->assertCsiSeq('24m', Actions::underline(false));
+        $this->assertCsiSeq('5m', Actions::slowBlink(true));
+        $this->assertCsiSeq('7m', Actions::reverse(true));
+        $this->assertCsiSeq('8m', Actions::hidden(true));
+        $this->assertCsiSeq('9m', Actions::strike(true));
+        $this->assertCsiSeq('2J', Actions::clear(ClearType::All));
+        $this->assertCsiSeq('3J', Actions::clear(ClearType::Purge));
+        $this->assertCsiSeq('J', Actions::clear(ClearType::FromCursorDown));
+        $this->assertCsiSeq('1J', Actions::clear(ClearType::FromCursorUp));
+        $this->assertCsiSeq('2K', Actions::clear(ClearType::CurrentLine));
+        $this->assertCsiSeq('K', Actions::clear(ClearType::UntilNewLine));
+        $this->assertCsiSeq('S', Actions::scrollUp());
+        $this->assertCsiSeq('T', Actions::scrollDown());
+
+        $this->assertOscSeq("0;Hello\x07", Actions::setTitle('Hello'));
+
         $this->assertRawSeq("\x1B[?1000h\x1B[?1002h\x1B[?1003h\x1B[?1015h\x1B[?1006h", Actions::enableMouseCapture());
         $this->assertRawSeq("\x1B[?1006h\x1B[?1015h\x1B[?1003h\x1B[?1002h\x1B[?1000h", Actions::disableMouseCapture());
     }
+    private function assertCsiSeq(string $string, Action $command): void
+    {
+        $this->assertSeq("\033[", $string, $command);
+    }
 
-    private function assertAnsiCode(string $string, Action $command): void
+    private function assertOscSeq(string $string, Action $command): void
+    {
+        $this->assertSeq("\033]", $string, $command);
+    }
+
+    private function assertSeq(string $prefix, string $string, Action $command): void
     {
         $writer = BufferWriter::new();
         $term = AnsiPainter::new($writer);
         $term->paint([$command]);
-        self::assertEquals(json_encode(sprintf("\033[%s", $string)), json_encode($writer->toString()), $command::class);
+        self::assertEquals(json_encode(sprintf('%s%s', $prefix, $string)), json_encode($writer->toString()), $command::class);
         $parsedCommand = AnsiParser::parseString($writer->toString(), true);
         self::assertEquals($command, $parsedCommand[0], 'parsing output');
     }
