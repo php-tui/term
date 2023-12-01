af252852e13237bda288ebd637e8aa25a1a06d40
Daniel Leech <daniel@dantleech.com>
2023-12-01 14:12:11 +0000

    Make restores the terminal state

--- example/events.php
+++ example/events.php
@@ -11,27 +11,37 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $terminal = Terminal::new();
 echo 'Entering raw mode...' . "\n";
+
 $terminal->enableRawMode();
 $terminal->execute(Actions::printString('Entering event loop, press ESC to exit'));
 $terminal->execute(Actions::moveCursorNextLine());
+$terminal->execute(Actions::enableMouseCapture());
+try {
+    eventLoop($terminal);
+} finally {
+    $terminal->execute(Actions::disableMouseCapture());
+    $terminal->disableRawMode();
+}
 
-while (true) {
-    while ($event = $terminal->events()->next()) {
-        $terminal->queue(Actions::printString($event->__toString()));
-        $terminal->queue(Actions::moveCursorNextLine());
-        $terminal->flush();
-        if ($event instanceof CodedKeyEvent) {
-            if ($event->code === KeyCode::Esc) {
-                $terminal->disableRawMode();
-                exit('esc pressed');
+function eventLoop(Terminal $terminal): void
+{
+    while (true) {
+        while ($event = $terminal->events()->next()) {
+            $terminal->queue(Actions::printString($event->__toString()));
+            $terminal->queue(Actions::moveCursorNextLine());
+            $terminal->flush();
+            if ($event instanceof CodedKeyEvent) {
+                if ($event->code === KeyCode::Esc) {
+                    return;
+                }
+            }
+            if ($event instanceof CharKeyEvent) {
+                if ($event->char === 'c' && $event->modifiers === KeyModifiers::CONTROL) {
+                    $terminal->disableRawMode();
+                    return;
+                }
             }
         }
-        if ($event instanceof CharKeyEvent) {
-            if ($event->char === 'c' && $event->modifiers === KeyModifiers::CONTROL) {
-                $terminal->disableRawMode();
-                exit('ctrl-c pressed');
-            }
-        }
+        usleep(10000);
     }
-    usleep(10000);
 }
